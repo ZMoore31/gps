fee1618f47ab379b5eda1b256e0898ad9819bc97
Zack Moore <ZMoore31@gmail.com>
2018-08-26 20:54:15 -0600

    start structuring files

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const helmet = require("helmet");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page!</h1>");
+});
+
 app.get("*", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.send("<h1>Missing Page!</h1>");
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
