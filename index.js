cacde63dae1f7d685895119419c7146795ccde86
Zack Moore <ZMoore31@gmail.com>
2018-08-27 18:09:49 -0600

    initial ejs setup

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const helmet = require("helmet");
 
+const routes = require("./routes/index.js");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("<h1>Home Page!</h1>");
-});
+routes(app);
 
 app.get("*", (req, res) => {
   res.send("<h1>Missing Page!</h1>");
