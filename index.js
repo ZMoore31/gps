f8b2c2f0b604300515f756d66c92a8cba7fae9a1
Zack Moore <ZMoore31@gmail.com>
2018-08-27 20:30:35 -0600

    finish homepage

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.set("view engine", "ejs");
 
 routes(app);
